2c9cf4fe674d0c8387a48a8d987d20211c6b1589
Lucas-Sholtz <misteriphone7.7.7@gmail.com>
2021-05-26 23:59:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NonLinearEquationsLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
