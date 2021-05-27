7ecd7343ff9321df41244de5d503d2c06ad0ef5c
Lucas-Sholtz <misteriphone7.7.7@gmail.com>
2021-05-27 23:50:33 +0300

    Large system and relaxation

--- NLE.cs
+++ NLE.cs
@@ -25,7 +25,7 @@ namespace NonLinearEquationsLab
         public static Func<Double[], double>[] GetNFunctions(int n)
         {
             Func<Double[], double>[] funcs = new Func<Double[], double>[n];
-            for(int i = 0; i < funcs.Length; i++)
+            for (int i = 0; i < funcs.Length; i++)
             {
                 Func<Double[], double> f = p =>
                 {
@@ -74,7 +74,7 @@ namespace NonLinearEquationsLab
             }
             for (int j = 1; j <= p.Length; j++)
             {
-                if (j != step+1)
+                if (j != step + 1)
                 {
                     value -= Math.Pow(j, 2);
                 }
@@ -85,14 +85,58 @@ namespace NonLinearEquationsLab
             }
             return value;
         }
+        private static double GetNFunctionValue(int n, double[] p)
+        {
+            double value = 0;
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (i == n)
+                {
+                    value += Math.Pow(p[i], 3);
+                }
+                else
+                {
+                    value += Math.Pow(p[i], 2);
+                }
+            }
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (i == n)
+                {
+                    value -= Math.Pow((i + 1), 3);
+                }
+                else
+                {
+                    value -= Math.Pow((i + 1), 2);
+                }
+            }
+            return value;
+        }
+        private static double GetNDerivativeValue(int n, double[] p, int index)
+        {
+            // x1^3 x2^2 ... xn^2 = 1 + 4 + ... + n^2
+            // x1^2 x2^3 ... xn^2 = 1 + 8 + ... + n^2
+            // ......................................
+            // x1^2 x2^2 ... xn^3 = 1 + 4 + ... + n^3
+
+            // and their derivatives
+            if (n == index)
+            {
+                return 3 * Math.Pow(p[n], 2);
+            }
+            else
+            {
+                return 2 * p[n];
+            }
+        }
         public static Func<Double[], double>[][] Derivatives(Func<Double[], double>[] f)
         {
             var jacobian = new Func<Double[], double>[f.Length][];
 
-            for(int i = 0; i < f.Length; i++)
+            for (int i = 0; i < f.Length; i++)
             {
                 jacobian[i] = new Func<double[], double>[f.Length];
-                for(int j = 0; j < f.Length; j++)
+                for (int j = 0; j < f.Length; j++)
                 {
                     jacobian[i][j] = Differentiate.FirstPartialDerivativeFunc(f[i], j);
                 }
@@ -136,7 +180,7 @@ namespace NonLinearEquationsLab
 
             } while ((xnp1 - xn).L2Norm() > eps);
 
-            Console.WriteLine($"Answer:\n {xnp1.ToVectorString()}");
+            Console.WriteLine($"Answer:\n{xnp1.ToVectorString()}");
 
             var delta = Vector<double>.Build.Dense(f.Length, 0);
             for (int i = 0; i < delta.Count; i++)
@@ -144,7 +188,52 @@ namespace NonLinearEquationsLab
                 delta[i] = f[i].Invoke(xnp1.ToArray());
             }
 
-            Console.WriteLine($"Delta vector:\n {xnp1.ToVectorString()}");
+            Console.WriteLine($"Delta vector:\n{xnp1.ToVectorString()}");
+            Console.WriteLine($"Delta norm:{delta.L2Norm()}");
+        }
+        public static void RunModifiedNewtonMethod(int n)
+        {
+            Vector<double> xn = Vector<double>.Build.Dense(n, 0);
+            Vector<double> xnp1 = Vector<double>.Build.Random(n);
+            Matrix<double> A = Matrix<double>.Build.Dense(n, n, 0);
+            Vector<double> F = Vector<double>.Build.Dense(n, 0);
+
+            do
+            {
+                xn = xnp1;
+                for (int i = 0; i < A.RowCount; i++)
+                {
+                    for (int j = 0; j < A.ColumnCount; j++)
+                    {
+                        A[i, j] = GetNDerivativeValue(j, xn.ToArray(), i);
+                    }
+                }
+                Console.WriteLine("A:");
+                Console.WriteLine(A.ToMatrixString());
+                var invertA = A.Inverse();
+                Console.WriteLine("Inverse A:");
+                Console.WriteLine(invertA.ToMatrixString());
+                for (int i = 0; i < F.Count; i++)
+                {
+                    F[i] = GetNFunctionValue(i, xn.ToArray());
+                }
+                Console.WriteLine("F:");
+                Console.WriteLine(F.ToVectorString());
+                xnp1 = xn - invertA * F;
+                Console.WriteLine("Xn+1:");
+                Console.WriteLine(xnp1.ToVectorString());
+
+            } while ((xnp1 - xn).L2Norm() > eps);
+
+            Console.WriteLine($"Answer:\n{xnp1.ToVectorString()}");
+
+            var delta = Vector<double>.Build.Dense(n, 0);
+            for (int i = 0; i < delta.Count; i++)
+            {
+                delta[i] = GetNFunctionValue(i, xnp1.ToArray());
+            }
+
+            Console.WriteLine($"Delta vector:\n{delta.ToVectorString()}");
             Console.WriteLine($"Delta norm: {delta.L2Norm()}");
         }
         public static void RunNewtonMethod(Func<Double[], double>[] f)
@@ -194,6 +283,140 @@ namespace NonLinearEquationsLab
 
             Console.WriteLine($"Delta vector:\n {z.ToVectorString()}");
             Console.WriteLine($"Delta norm: {z.L2Norm()}");
+        }
+        public static void RunNewtonMethod(int n)
+        {
+            Vector<double> x = Vector<double>.Build.Random(n);
+            Matrix<double> A = Matrix<double>.Build.Dense(n, n, 0);
+            Vector<double> F = Vector<double>.Build.Dense(n, 0);
+            Vector<double> z = Vector<double>.Build.Dense(n, 0);
+
+            do
+            {
+                for (int i = 0; i < A.RowCount; i++)
+                {
+                    for (int j = 0; j < A.ColumnCount; j++)
+                    {
+                        A[i, j] = GetNDerivativeValue(j, x.ToArray(), i);
+                    }
+                }
+                Console.WriteLine("A:");
+                Console.WriteLine(A.ToMatrixString());
+                for (int i = 0; i < F.Count; i++)
+                {
+                    F[i] = GetNFunctionValue(i, x.ToArray());
+                }
+                Console.WriteLine("F:");
+                Console.WriteLine(F.ToVectorString());
+                z = A.Solve(F);
+                Console.WriteLine("solve A*z=F \nz:");
+                Console.WriteLine(z.ToVectorString());
+                Console.WriteLine("z norm:");
+                Console.WriteLine(z.L2Norm());
+                x = x - z;
+                Console.WriteLine("new x:");
+                Console.WriteLine(x.ToVectorString());
+
+            } while (z.L2Norm() > eps);
+
+            Console.WriteLine($"Answer:\n {x.ToVectorString()}");
+
+            var delta = Vector<double>.Build.Dense(n, 0);
+            for (int i = 0; i < F.Count; i++)
+            {
+                F[i] = GetNFunctionValue(i, z.ToArray());
+            }
+
+            Console.WriteLine($"Delta vector:\n{z.ToVectorString()}");
+            Console.WriteLine($"Delta norm:{z.L2Norm()}");
+        }
+        public static void RunRelaxationMethod(Func<Double[], double>[] f)
+        {
+            Vector<double> xk = Vector<double>.Build.Random(f.Length);
+            Console.WriteLine("Enter x vector:");
+            for (int i = 0; i < xk.Count; i++)
+            {
+                xk[i] = double.Parse(Console.ReadLine());
+            }
+
+            Vector<double> xkp1 = Vector<double>.Build.Random(f.Length);
+
+            Vector<double> F = Vector<double>.Build.Dense(f.Length, 0);
+            for (int i = 0; i < F.Count; i++)
+            {
+                F[i] = f[i].Invoke(xk.ToArray());
+            }
+
+            double t = 0.1;
+
+            //первое приближение
+            xkp1 = xk - t * F;
+            Console.WriteLine("First approximation:");
+            Console.WriteLine(xkp1.ToVectorString());
+            do
+            {
+                xk = xkp1;
+                for (int i = 0; i < F.Count; i++)
+                {
+                    F[i] = f[i].Invoke(xk.ToArray());
+                }
+                Console.WriteLine(xkp1.ToVectorString());
+                xkp1 = xk - t * F;
+            } while ((xkp1 - xk).L2Norm() > eps);
+
+            Console.WriteLine(xkp1);
+
+            var delta = Vector<double>.Build.Dense(f.Length, 0);
+            for (int i = 0; i < F.Count; i++)
+            {
+                delta[i] = f[i].Invoke(xkp1.ToArray());
+            }
+            Console.WriteLine($"Delta vector:\n{delta.ToVectorString()}");
+            Console.WriteLine($"Delta norm:{delta.L2Norm()}");
+        }
+        public static void RunRelaxationMethod(int n)
+        {
+            Vector<double> xk = Vector<double>.Build.Random(n);
+            Console.WriteLine("Enter x vector:");
+            for (int i = 0; i < xk.Count; i++)
+            {
+                xk[i] = double.Parse(Console.ReadLine());
+            }
+
+            Vector<double> xkp1 = Vector<double>.Build.Random(n);
+
+            Vector<double> F = Vector<double>.Build.Dense(n);
+            for (int i = 0; i < F.Count; i++)
+            {
+                F[i] = GetNFunctionValue(i, xk.ToArray());
+            }
+
+            double t = 0.00001;
+                
+            //первое приближение
+            xkp1 = xk - t * F;
+            Console.WriteLine("First approximation:");
+            Console.WriteLine(xkp1.ToVectorString());
+            do
+            {
+                xk = xkp1;
+                for (int i = 0; i < F.Count; i++)
+                {
+                    F[i] = GetNFunctionValue(i, xk.ToArray());
+                }
+                Console.WriteLine(xkp1.ToVectorString());
+                xkp1 = xk - t * F;
+            } while ((xkp1 - xk).L2Norm() > eps);
+
+            Console.WriteLine(xkp1);
+
+            var delta = Vector<double>.Build.Dense(n, 0);
+            for (int i = 0; i < F.Count; i++)
+            {
+                delta[i] = GetNFunctionValue(i, xk.ToArray());
+            }
+            Console.WriteLine($"Delta vector:\n{delta.ToVectorString()}");
+            Console.WriteLine($"Delta norm:{delta.L2Norm()}");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,8 +6,12 @@ namespace NonLinearEquationsLab
     {
         static void Main(string[] args)
         {
-            NLE.RunNewtonMethod(NLE.GetNFunctions(10));
+            //NLE.RunNewtonMethod(5);
+            //NLE.RunNewtonMethod(NLE.GetTwoFunctions());
+            //NLE.RunModifiedNewtonMethod(5);
             //NLE.RunModifiedNewtonMethod(NLE.GetTwoFunctions());
+            NLE.RunRelaxationMethod(10);
+            //NLE.RunRelaxationMethod(NLE.GetTwoFunctions());
         }
     }
 }
